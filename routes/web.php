10ce2bddc16bad8ad0eba80e6429fa390bbde373
Megacy777 <megacy777@gmail.com>
2024-03-04 15:16:40 +0300

    Installing Laravel Breeze
    Working with Breeze dealing with Authentication routes, dashboard, controllers, models, and Components for views

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
 use App\Http\Controllers\ListItemController;
 use App\Http\Controllers\AuctionController;
 use App\Http\Controllers\AuctioneerController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,6 +20,20 @@ use App\Http\Controllers\AuctioneerController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
 
 Route::post('/saveItem', function () {
     return view('saveItem');
